6b4386505c38212e999cf391f0c107dd6c040e93
Petchy77 <petch.natawat@gmail.com>
2023-10-15 19:45:35 +0700

    First commit

---
+++ index.js
@@ -0,0 +1,77 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const app = express();
+const PORT = 3000;
+app.use(bodyParser.json());
+
+// ติดต่อ MongoDB
+mongoose.connect('mongodb://localhost:27017/productDB', {useNewUrlParser: true, useUnifiedTopology: true});
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+// สร้าง Schema และ Model
+const productSchema = new mongoose.Schema({
+  name: String,
+  category: String,
+  price: Number,
+  stock: Number
+});
+
+const Product = mongoose.model('Product', productSchema);
+
+// Read all products
+app.get('/products', async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.json(products);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+// Read a single product by ID
+app.get('/products/:id', async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    if (!product) throw new Error('Product not found');
+    res.json(product);
+  } catch (err) {
+    res.status(404).json({ error: err.message });
+  }
+});
+
+// Create a product
+app.post('/products', async (req, res) => {
+  try {
+    const newProduct = await Product.create(req.body);
+    res.json(newProduct);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+});
+
+// Update a product by ID
+app.put('/products/:id', async (req, res) => {
+  try {
+    const updatedProduct = await Product.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    res.json(updatedProduct);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+});
+
+// Delete an item by ID
+app.delete('/products/:id', async (req, res) => {
+  try {
+    const deletedProduct = await Product.findByIdAndDelete(req.params.id);
+    res.json(deletedProduct);
+  } catch (err) {
+    res.status(404).json({ error: err.message });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
